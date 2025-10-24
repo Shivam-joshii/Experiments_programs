fa2a739e439f4309a2a9d8839b04617eee153971
Shivam-joshii <shivam.jooshii@gmail.com>
2025-10-24 07:51:06 +0530

    Add book information system in info.c

---
+++ info.c
@@ -0,0 +1,24 @@
+Book Information System:
+
+
+#include <stdio.h>
+int main() {
+    char title[50], author[50], publisher[50];
+    int isbn, year;
+    float price;
+    printf("Enter Book Title: ");
+    scanf("%s", title);
+    printf("Enter Author Name: ");
+    scanf("%s", author);
+    printf("Enter ISBN: ");
+    scanf("%d", &isbn);
+    printf("Enter Price: ");
+    scanf("%f", &price);
+    printf("Enter Publisher: ");
+    scanf("%s", publisher);
+    printf("Enter Year of Publication: ");
+    scanf("%d", &year);
+    printf("\nBook Information:\nTitle: %s\nAuthor: %s\nISBN: %d\nPrice: %.2f\nPublisher: %s\nYear: %d\n",
+           title, author, isbn, price, publisher, year);
+    return 0;
+}
