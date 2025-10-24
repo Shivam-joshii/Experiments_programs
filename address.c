31b260f100c10d28ab057c599065020b5284133a
Shivam-joshii <shivam.jooshii@gmail.com>
2025-10-24 07:46:30 +0530

    Add program to print variable address and value

---
+++ address.c
@@ -0,0 +1,10 @@
+Print Address of a Variable:
+
+
+#include <stdio.h>
+int main() {
+    int var = 10;
+    printf("Value of var = %d\n", var);
+    printf("Address of var = %p\n", &var);
+    return 0;
+}
